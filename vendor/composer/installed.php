4508c0eb4f2a0583a98cc45b52c4e0e53ca912d9
Daniel-Pocai <85628850+Daniel-Pocai@users.noreply.github.com>
2024-10-29 23:01:39 -0300

    att 4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daniel-pocai/pratic-agro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e8bf5254774a90f01eab1f86b6cf9d7de437e1f6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daniel-pocai/pratic-agro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e8bf5254774a90f01eab1f86b6cf9d7de437e1f6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yiisoft/yii' => array(
+            'pretty_version' => '1.1.30',
+            'version' => '1.1.30.0',
+            'reference' => '5f760e818b8bc6e4c11b6cd36d9549892fe4747b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yiisoft/yii',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
